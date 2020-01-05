f5755633fdd0a21e8f2a9dc3362bb5002280552b
Freimans <freimans@post.bgu.ac.il>
2020-01-05 23:40:09 +0200

    CreditCard class and Guardian class created

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,2 +1,14 @@
 public class CreditCard {
+
+    String creditCardNumber;
+    int expirationYear;
+    int expirationMonth;
+    String safetyDigits;
+
+    public CreditCard(String creditCardNumber, int expirationYear, int expirationMonth, String safetyDigits) {
+        this.creditCardNumber = creditCardNumber;
+        this.expirationYear = expirationYear;
+        this.expirationMonth = expirationMonth;
+        this.safetyDigits = safetyDigits;
+    }
 }
--- src/Guardian.java
+++ src/Guardian.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Guardian {
+    List<ChildCard> childList;
+    List<CreditCard> creditCards;
+    Map map;
+
+    public Guardian(Map map)
+    {
+        this.map = map;
+        this.childList = new ArrayList<>();
+        this.creditCards = new ArrayList<>();
+    }
+
+    public void addChild(ChildCard child)
+    {
+        if(child != null)
+            this.childList.add(child);
+    }
+
+    public void addCreditCard(CreditCard card)
+    {
+        if(card != null)
+            this.creditCards.add(card);
+    }
 }
