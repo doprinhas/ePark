f794957013cbf39a1300c56b1b7c3d9fe14f0e0a
Oscar Epshtein <Oscareps>
2020-01-05 23:36:24 +0200

    Adding Ride Class Shahar haomo

--- src/Ride.java
+++ src/Ride.java
@@ -1,2 +1,37 @@
 public class Ride {
+
+
+    private WeightRestriction weightRestriction;
+    private HeightRestriction heightRestriction;
+    private AgeRestriction ageRestriction;
+
+    public Ride(int weightRestriction, int heightRestriction, int ageRestriction) {
+        this.weightRestriction = new WeightRestriction(weightRestriction);
+        this.heightRestriction = new HeightRestriction(heightRestriction);
+        this.ageRestriction = new AgeRestriction(ageRestriction);
+    }
+
+    public WeightRestriction getWeightRestriction() {
+        return weightRestriction;
+    }
+
+    public void setWeightRestriction(int weightRestriction) {
+        this.weightRestriction =  new WeightRestriction(weightRestriction);
+    }
+
+    public HeightRestriction getHeightRestriction() {
+        return heightRestriction;
+    }
+
+    public void setHeightRestriction(int heightRestriction) {
+        this.heightRestriction = new HeightRestriction(heightRestriction);
+    }
+
+    public AgeRestriction getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(int ageRestriction) {
+        this.ageRestriction = new AgeRestriction(ageRestriction);
+    }
 }
